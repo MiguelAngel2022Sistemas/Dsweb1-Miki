6f2eed7bf43f0521853f49cafdc0a380973a84f5
Wilder Julio Espinoza Bravo <wilderespinoza@bcp.com.pe>
2023-06-08 18:38:10 -0500

    proyecto java recien inicializado

---
+++ dsw1-soap-2023/src/main/java/edu/idat/dsm/dsw1soap2023/Dsw1Soap2023Application.java
@@ -0,0 +1,13 @@
+package edu.idat.dsm.dsw1soap2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1Soap2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1Soap2023Application.class, args);
+	}
+
+}
---
+++ dsw1-soap-2023/src/test/java/edu/idat/dsm/dsw1soap2023/Dsw1Soap2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.idat.dsm.dsw1soap2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1Soap2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
